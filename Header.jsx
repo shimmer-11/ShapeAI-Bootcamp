1968c247865d397a2f66d1f2239b427d972efba8
shimmer-11 <85435970+shimmer-11@users.noreply.github.com>
2021-06-07 12:02:51 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(){
+  return(
+    <header>
+      <h1> Shape AI Project</h1>
+      
+
+    </header>
+  );
+
+}
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js</h1>
+      <p>
+        this is amazing bootcamp. I really enjoy this bootcamp. It is really
+        helpfull for me. "Thankyou so much Shaurya."
+      </p>
+    </div>
+  );
+}
+export default Note;
